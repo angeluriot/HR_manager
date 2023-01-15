96330f976870fae94e2cfe8fe7105e170c5c95f8
N4tanael <82747654+N4tanael@users.noreply.github.com>
2023-01-15 16:22:42 +0100

    Update Menu.svelte

--- client/src/lib/components/Menu.svelte
+++ client/src/lib/components/Menu.svelte
@@ -1,20 +1,72 @@
 <script lang="ts">
+    import { link } from "svelte-spa-router";
+
+
+	let routes =  [{link:"/#/", item:"Accueil"},
+				  {link:"/#/statistics", item:"Statistiques"},
+				  {link:"/#/requests", item:"Demandes"},
+				  {link:"#/notifications", item:"Notifications"},
+				  {link:"/#/account", item:"Compte"},
+				  {link:"/#/settings", item:"Paramètres"}];
+
+	let activeItem = "Accueil";
+
+	const navChange = (item:string) => {
+		activeItem = item;
+		console.log(activeItem);
+	}
 </script>
 
 <nav class="fixed flex flex-col left-0 top-0 justify-start items-start">
 	<span class="text-2xl">PoPS2223</span>
-	<a href="/#/">Accueil</a>
+	{#each routes as route}
+            <a href={route.link} on:click={() => navChange(route.item)} class:active={route.item===activeItem}>
+				{route.item}
+			</a>
+    {/each}
+	<!-- <a href="/#/">Accueil</a>
 	<a href="/#/statistics">Statistiques</a>
 	<a href="/#/requests">Demandes</a>
 	<a href="/#/notifications">Notifications</a>
 	<a href="/#/account">Compte</a>
 	<a href="/#/settings">Paramètres</a>
-	<span>Prénom Nom</span>
+	<span>Prénom Nom</span> -->
+
+	<!-- {#each items as item}
+            <li on:click={() => dispatch('tabChange',item)}>
+                <div class:active={item===activeItem}>{item}</div>
+            </li>
+    {/each} -->
 </nav>
 
 <style>
-	nav {
-		width: 300px;
-		height: 100%;
-	}
+nav {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;height: 100%; /* Full height */
+  position: fixed; /* Make it stick, even on scroll */
+  overflow: auto; /* Enable scrolling if the sidenav has too much content */
+}
+
+a {
+  display: block;
+  width: 200px;
+  color: #000;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+}
+
+/* Change the link color on hover */
+a:hover {
+  background-color: #555;
+  color: white;
+}
+
+.active {
+  background-color: #007AFF;
+  color: white;
+}
 </style>
