3ff5245a9d8ad7f60a9cb54e8f007d6b106b4c8e
Emma Nourry <82103105+emmanry@users.noreply.github.com>
2023-01-19 17:34:03 +0100

    creation requestCard

---
+++ client/src/lib/components/RequestCard.svelte
@@ -0,0 +1,45 @@
+<script>
+    export let type;
+    export let state;
+    export let days;
+    export let start;
+    export let end;
+    export let comments;
+
+    export let colors = ["#FFFFFF", "#FF00FF", "#00FF00"];
+</script>
+
+<div id="card">
+    <div id="header">
+        <h2 id="title">{type}</h2>
+        <div id="state" style="--color : {colors[2]}">{state}</div>
+    </div>
+    {#if type == "Télétravail"}
+        Jours : {#each days as _day}{_day}{/each}
+    {/if}
+</div>
+
+<style>
+    #card{
+        background: #FFFFFF;
+        box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+        margin: 10px 10px 10px 10px;
+        padding: 5px 5px 5px 5px;
+        width: 20vw;
+        text-align: left;
+    }
+    #header{
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    #state{
+        background-color: var(--color);
+        box-shadow: 0px 6px 25px rgba(0, 0, 0, 0.25);
+        padding: 5px 5px 5px 5px;
+        border-radius: 15px;
+        font-size: small;
+    }
+</style>
--- client/src/lib/views/Requests.svelte
+++ client/src/lib/views/Requests.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import Request from "../components/Request.svelte";
+	import RequestCard from "../components/RequestCard.svelte";
+
+	let nbOfResquests = 4; // à recup de la bdd
+
 </script>
 
-<div class="main gap-10">
+<!-- <div class="main gap-10">
 	<h1>Demandes</h1>
 	<div class="flex flex-row gap-6">
 		<div class="flex flex-col gap-6">
@@ -17,7 +20,34 @@
 		</div>
 	</div>
 	<a href="/#/requests/new">Nouvelle demande</a>
+</div> -->
+  
+<div id="container" class="main gap-10">
+	<div id="sub-container">
+		<h1>Demandes personnelles</h1>
+		<div id="scrollable">
+			{#each Array(nbOfResquests) as _}
+				<RequestCard type={"Télétravail"} state={"Brouillon"} days={["Jeudi", "Mardi"]} start={"01/12/2022"} end={"31/12/2022"} comments={"None"}/>
+			{/each}
+		</div>
+	</div>
+	<div id="sub-container">
+		<h1>Demandes à valider</h1>
+
+	</div>
 </div>
 
 <style>
+	#container{
+		flex-direction: row;
+		display: flex;
+    	height: 100vh;
+		margin-left: 300px;
+	}
+	#sub-container{
+		height: 100vh;
+	}
+	#scrollable{
+		overflow: auto;
+	}
 </style>
