89408054142ad20d795e8c91059485941b483352
arthurazambre <arthur@azambre.net>
2023-01-23 00:20:08 +0100

    premiere_version

--- client/src/lib/components/Calendar.svelte
+++ client/src/lib/components/Calendar.svelte
@@ -1,9 +1,150 @@
 <script lang="ts">
+
+	export let day_names;
+	export let days;
+	export let absences;
+
 </script>
 
-<div>
-	<span>Calendrier</span>
+<div class="calendar_content">
+	{#each day_names as day_name}
+		<span class="day_name">{day_name}</span>
+	{/each}
+
+	{#each days as day}
+		{#if day.this_month}
+			<span class="day">{day.name}</span>
+		{:else}
+			<span class="day not_this_month">{day.name}</span>
+		{/if}
+	{/each}
+
+	{#each absences as absence}
+		<section class="absence_section {absence.type}" style="grid-row: {absence.start_row}; grid-column: {absence.start_col} / span {absence.section_duration}; top: {absence.position * 25 - 5}px">
+			<span class="absence_title">{absence.title}</span>
+		</section>
+	{/each}
 </div>
 
 <style>
+	.absence_section {
+		border-left-width: 3px;
+		border-left-style: solid;
+		font-size: 10px bold;
+		margin: 10px;
+		position: relative;
+		align-self: center;
+		z-index: 2;
+
+		border: 0;
+		border-radius: 14px;
+		color: white;
+	}
+
+	.conge {
+		background: #0AD443;
+	}
+
+	.maladie {
+		background: #F62942;
+	}
+
+	.physique {
+		background: #1640D4;
+	}
+
+	.absence_section
+
+	.absence_title {
+		font-size: 10px bold;
+	}
+
+	.calendar_content {
+		display: grid;
+		width: 100%;
+		height: 100%;
+		grid-template-columns: repeat(7, minmax(120px, 1fr));
+		grid-template-rows: 50px;
+		grid-auto-rows: 120px;
+		overflow: auto;
+	}
+
+	.day {
+		border: 1px solid #C1CBDA;
+		text-align: right;
+		padding: 14px 20px;
+		letter-spacing: 1px;
+		font-size: 12px;
+		box-sizing: border-box;
+		color: #526581;
+		position: relative;
+		z-index: 1;
+		padding-bottom: 87px; 					/* ADAPTER !!! */
+	}
+
+	.not_this_month {
+		color: #D3DDE7;
+	}
+
+	.day:nth-of-type(n + 1):nth-of-type(-n + 7) {
+		grid-row: 1;
+		border-bottom: 0;
+	}
+	.day:nth-of-type(n + 8):nth-of-type(-n + 14) {
+		grid-row: 2;
+		border-bottom: 0;
+	}
+	.day:nth-of-type(n + 15):nth-of-type(-n + 21) {
+		grid-row: 3;
+		border-bottom: 0;
+	}
+	.day:nth-of-type(n + 22):nth-of-type(-n + 28) {
+		grid-row: 4;
+		border-bottom: 0;
+	}
+	.day:nth-of-type(n + 29):nth-of-type(-n + 35) {
+		grid-row: 5;
+		border-bottom: 0;
+	}
+	.day:nth-of-type(n + 36):nth-of-type(-n + 42) {
+		grid-row: 6;
+	}
+	.day:nth-of-type(7n + 1) {
+		grid-column: 1/1;
+		border-right: 0;
+	}
+	.day:nth-of-type(7n + 2) {
+		grid-column: 2/2;
+		border-right: 0;
+	}
+	.day:nth-of-type(7n + 3) {
+		grid-column: 3/3;
+		border-right: 0;
+	}
+	.day:nth-of-type(7n + 4) {
+		grid-column: 4/4;
+		border-right: 0;
+	}
+	.day:nth-of-type(7n + 5) {
+		grid-column: 5/5;
+		border-right: 0;
+	}
+	.day:nth-of-type(7n + 6) {
+		grid-column: 6/6;
+		border-right: 0;
+	}
+	.day:nth-of-type(7n + 7) {
+		grid-column: 7/7;
+	}
+
+	.day_name {
+		font-size: 12px;
+		text-transform: uppercase;
+		color: white;
+		background-color: #007AFF;
+		text-align: center;
+		border-bottom: 1px solid rgba(166, 168, 179, 0.12);
+		line-height: 50px;
+		font-weight: 500;
+	}
 </style>
--- client/src/lib/views/Home.svelte
+++ client/src/lib/views/Home.svelte
@@ -1,11 +1,278 @@
 <script lang="ts">
+    import { TestimonialPlaceholder } from "flowbite-svelte";
 	import Calendar from "../components/Calendar.svelte";
+
+	var days = [];
+	let now = new Date();
+	let year = now.getFullYear();
+	let month = now.getMonth();
+
+	let month_names = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+	var all_absences = [
+		{title: "A. Didot (RTT)", date: new Date(2023, 0, 2), section_date: new Date(2023, 0, 2), duration: 3, section_duration: 3, start_row: 0, start_col: 0, position: -1, type: "conge"},
+		{title: "C. Moray (AM)", date: new Date(2023, 0, 2), section_date: new Date(2023, 0, 2), duration: 2, section_duration: 2, start_row: 0, start_col: 0, position: -1, type: "maladie"},
+		{title: "T. Truc (AM)", date: new Date(2023, 0, 4), section_date: new Date(2023, 0, 4), duration: 2, section_duration: 2, start_row: 0, start_col: 0, position: -1, type: "maladie"},
+		{title: "T. Truc2 (T)", date: new Date(2023, 0, 3), section_date: new Date(2023, 0, 3), duration: 2, section_duration: 2, start_row: 0, start_col: 0, position: -1, type: "physique"},
+		{title: "T. Truc4 (T)", date: new Date(2023, 1, 7), section_date: new Date(2023, 1, 7), duration: 2, section_duration: 2, start_row: 0, start_col: 0, position: -1, type: "physique"},
+		{title: "T. Truc3 (T)", date: new Date(2023, 0, 10), section_date: new Date(2023, 0, 10), duration: 7, section_duration: 7, start_row: 0, start_col: 0, position: -1, type: "physique"}
+	];
+
+	var all_absences_per_week = [];
+
+	var absences = [];
+
+	var is_month_mode = true;
+
+	$:month, year, update_calendar();
+
+	function update_calendar()
+	{
+		days = [];
+		all_absences_per_week = [];
+		absences = [];
+
+		var first_day = new Date(year, month, 0).getDay();
+		var days_in_this_month = new Date(year, month + 1, 0).getDate();
+		var days_in_last_month = new Date(year, month, 0).getDate();
+		var prev_month = month == 0 ? 11 : month - 1;
+
+		for (let i = days_in_last_month - first_day; i < days_in_last_month; i++)
+		{
+			let d = new Date(prev_month == 11 ? year - 1 : year, prev_month, i + 1);
+			days.push({name: '' + (i + 1), this_month: false, date: d});
+		}
+
+		for (let i = 0; i < days_in_this_month; i++)
+		{
+			let d = new Date(year, month, i + 1);
+			days.push({name: '' + (i + 1), this_month: true, date: d});
+		}
+
+		for (let i = 0; days.length % 7; i++)
+		{
+			let d = new Date((month == 11 ? year + 1 : year), (month + 1) % 12, i + 1);
+			days.push({name: ' ' + (i + 1), this_month: false, date: d});
+		}
+
+		for (let absence of all_absences)
+		{
+			let current_duration = absence.duration;
+			let this_section_duration = absence.section_duration;
+			let this_section_date = absence.section_date;
+
+			if (current_duration > 5 - this_section_date.getDay())
+			{
+				while (current_duration > 5 - this_section_date.getDay())
+				{
+					this_section_duration = 5 - this_section_date.getDay();
+					current_duration -= this_section_duration;
+					all_absences_per_week.push({title: absence.title, date: absence.date, section_date: this_section_date, duration: absence.duration, section_duration: this_section_duration, start_row: 0, start_col: 0, position: -1, type: absence.type});
+					this_section_date = new Date(this_section_date.getFullYear(), this_section_date.getMonth(), this_section_date.getDate() + 7 - this_section_date.getDay());
+				}
+
+				all_absences_per_week.push({title: absence.title, date: absence.date, section_date: this_section_date, duration: absence.duration, section_duration: current_duration, start_row: 0, start_col: 0, position: -1, type: absence.type});
+			}
+
+			else
+				all_absences_per_week.push(absence);
+		}
+
+		for (let absence of all_absences_per_week)
+		{
+			if (absence.section_date.getFullYear() == year && absence.section_date.getMonth() == month)
+			{
+				absence.start_row = Math.floor((absence.section_date.getDate() + first_day) / 7) + 2
+				absence.start_col = (absence.section_date.getDate() + first_day) % 7 + 1;
+
+				absences.push(absence);
+			}
+		}
+
+		for (let a of absences)
+		{
+			let i = 0;
+			let position_found = false;
+
+			while (!(position_found) && i < 3)
+			{
+				let position_ok = true;
+				for (let b of absences)
+				{
+					if (a.title != b.title && b.position == i && are_overlayed(a.date.getDate(), a.duration, b.date.getDate(), b.duration))
+					{
+						position_ok = false;
+						break;
+					}
+				}
+
+				if (position_ok)
+				{
+					position_found = true;
+					a.position = i;
+				}
+
+				i++;
+			}
+		}
+	}
+
+	function are_overlayed(date1: number, duration1: number, date2: number, duration2: number)
+	{
+		if (date1 < date2)
+			return duration1 > date2 - date1;
+
+		return duration2 > date1 - date2;
+	}
+
+	function left()
+	{
+		if (month==0)
+		{
+			month=11;
+			year--;
+		}
+		else
+			month--;
+	}
+
+	function right()
+	{
+		if (month == 11)
+		{
+			month = 0;
+			year++;
+		}
+		else
+			month++;
+	}
+
+	function week_mode()
+	{
+		if (!is_month_mode)
+			return;
+
+		is_month_mode = false;
+
+		let week_mode_button = document.getElementById("week_mode_button");
+		let month_mode_button = document.getElementById("month_mode_button");
+
+		week_mode_button.classList.remove("mode_button");
+		week_mode_button.classList.toggle("mode_button_checked");
+		month_mode_button.classList.remove("mode_button_checked");
+		month_mode_button.classList.toggle("mode_button");
+
+		week_mode_button.style.left = "10px";
+		month_mode_button.style.left = "125px";
+	}
+
+	function month_mode()
+	{
+		if (is_month_mode)
+			return;
+
+		is_month_mode = true;
+
+		let week_mode_button = document.getElementById("week_mode_button");
+		let month_mode_button = document.getElementById("month_mode_button");
+
+		week_mode_button.classList.remove("mode_button_checked");
+		week_mode_button.classList.toggle("mode_button");
+		month_mode_button.classList.remove("mode_button");
+		month_mode_button.classList.toggle("mode_button_checked");
+		week_mode_button.style.left = "20px";
+		month_mode_button.style.left = "115px";
+	}
+
 </script>
 
-<div class="main gap-10">
-	<h1>Accueil</h1>
-	<Calendar/>
+<div id="container" class="main gap-10">
+	<div class="calendar">
+		<div class="calendar_header">
+			<input type="button" on:click={() => week_mode()} class="mode_button" id="week_mode_button" value="Semaines">
+			<input type="button" on:click={() => month_mode()} class="mode_button_checked" id="month_mode_button" value="Mois">
+
+			<button class="months_button" on:click={() => left()}>&lt;</button>
+			<h1 id="titre">{month_names[month] + ' ' + year}</h1>
+			<button class="months_button" on:click={() => right()}>&gt;</button>
+			<span id="days_info">Nombre de jours de congé : 0 </span>
+		</div>
+		<Calendar day_names = {["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]} {days} {absences}/>
+	</div>
 </div>
 
 <style>
+	.calendar {
+		width: 100%;
+		height: 95%;
+		margin: auto;
+		overflow: hidden;
+		border-radius: 10px;
+		max-width: 1200px;
+	}
+
+	.calendar_header {
+		display: flex;
+		flex-direction: row;
+		text-align: center;
+		border-bottom: 1px solid rgba(166, 168, 179, 0.12);
+		padding: 30px 0;
+	}
+
+	.mode_button {
+		position: absolute;
+		font-size: 20px;
+
+		color: black;
+		cursor: pointer;
+	}
+
+	.mode_button_checked {
+		position: absolute;
+		font-size: 20px;
+
+		color: white;
+		background: #007AFF;
+		border-radius: 14px;
+		padding-left: 10px;
+		padding-right: 10px;
+		cursor: pointer;
+	}
+
+	.months_button {
+		border: 1px;
+		padding-left: 7px;
+		padding-right: 10px;
+		cursor: pointer;
+	}
+
+	.calendar_header h1 {
+		font-size: 25px;
+	}
+
+	#container {
+		position: relative;
+		flex-direction: column;
+		display: flex;
+		height: 100vh;
+		margin-left: 300px;
+	}
+
+	#month_mode_button {
+		left: 125px;
+	}
+
+	#week_mode_button {
+		left: 20px;
+	}
+
+	#days_info {
+		position: absolute;
+		right: 2%;
+		font-size: 20px;
+	}
+
+	#titre {
+		color: #09244B;
+	}
 </style>
