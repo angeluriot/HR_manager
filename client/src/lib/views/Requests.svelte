75d0d0a23884806daa06b8d91849ec15e6c0a433
Emma Nourry <82103105+emmanry@users.noreply.github.com>
2023-01-25 15:20:16 +0100

    rectifications css

--- client/src/lib/views/Requests.svelte
+++ client/src/lib/views/Requests.svelte
@@ -24,13 +24,23 @@
 	<a href="/#/requests/new">Nouvelle demande</a>
 </div> -->
   
-<div class="main gap-10 flex flex-col w-full">
-	<div class="flex flex-row">
+<div class="main flex flex-col w-full">
+	<div class="gap-10 flex flex-row">
 		<h1>Demandes personnelles</h1>
 		<h1>Demandes à valider</h1>
 	</div>
-	<div class="flex flex-row !items-start ">
+	<div class="gap-10 flex flex-row">
+		<div class="overflow-auto h-[50%] ">
+			{#each Array(nbOfResquests_personal) as _}
+				<p>haha</p>
+			{/each}
+		</div>
 		<div class="overflow-auto h-[50%]">
+			{#each Array(nbOfResquests_personal) as _}
+				<p>haha</p>
+			{/each}
+		</div>
+		<!-- <div class="overflow-auto h-[50%]">
 			{#each Array(nbOfResquests_personal) as _}
 				<RequestCard id={101} type={"Télétravail"} first_name={""} last_name={""} state={"Brouillon"} days={["Jeudi", "Mardi"]} start={"01/12/2022"} end={"31/12/2022"} comments={"None"}/>
 			{/each}
@@ -39,7 +49,7 @@
 			{#each Array(nbOfResquests_toValidate) as _}
 				<RequestCard id={23} type={"Télétravail"} first_name={"Jean"} last_name={"Dupont"} state={"Brouillon"} days={["Jeudi", "Mardi"]} start={"01/12/2022"} end={"31/12/2022"} comments={"None"}/>
 			{/each}
-		</div>
+		</div> -->
 	</div>
 	<a href="/#/requests/new">Nouvelle demande</a>
 </div>
