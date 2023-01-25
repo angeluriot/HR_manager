b67576eaf461d8d5b9e47cd7a11bb4c4c6e47072
Emma Nourry <82103105+emmanry@users.noreply.github.com>
2023-01-25 14:59:11 +0100

    tests chelou

--- client/src/lib/views/Requests.svelte
+++ client/src/lib/views/Requests.svelte
@@ -24,40 +24,26 @@
 	<a href="/#/requests/new">Nouvelle demande</a>
 </div> -->
   
-<div id="container" class="main gap-10 w-full">
-	<div id="sub-container">
+<div class="main gap-10 flex flex-col w-full">
+	<div class="flex flex-row">
 		<h1>Demandes personnelles</h1>
-		<div id="scrollable">
+		<h1>Demandes à valider</h1>
+	</div>
+	<div class="flex flex-row !items-start ">
+		<div class="overflow-auto h-[50%]">
 			{#each Array(nbOfResquests_personal) as _}
 				<RequestCard id={101} type={"Télétravail"} first_name={""} last_name={""} state={"Brouillon"} days={["Jeudi", "Mardi"]} start={"01/12/2022"} end={"31/12/2022"} comments={"None"}/>
 			{/each}
 		</div>
-	</div>
-	<div id="sub-container">
-		<h1>Demandes à valider</h1>
-		
-		<div id="scrollable">
+		<div class="overflow-auto h-[50%]">
 			{#each Array(nbOfResquests_toValidate) as _}
 				<RequestCard id={23} type={"Télétravail"} first_name={"Jean"} last_name={"Dupont"} state={"Brouillon"} days={["Jeudi", "Mardi"]} start={"01/12/2022"} end={"31/12/2022"} comments={"None"}/>
 			{/each}
 		</div>
 	</div>
+	<a href="/#/requests/new">Nouvelle demande</a>
 </div>
 
 <style>
-	h1{
-		margin: 20px 20px 20px 20px;
-	}
-	#container{
-		flex-direction: row;
-		display: flex;
-    	height: 100vh;
-		margin-left: 300px;
-	}
-	#sub-container{
-		height: 100vh;
-	}
-	#scrollable{
-		overflow: auto;
-	}
+
 </style>
