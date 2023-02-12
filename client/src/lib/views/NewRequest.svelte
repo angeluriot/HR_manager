55ca0943227193a4f2e7468f30c7d73cdaa8c252
N4tanael <82747654+N4tanael@users.noreply.github.com>
2023-02-12 17:30:42 +0100

    Tout en une demande

--- client/src/lib/views/NewRequest.svelte
+++ client/src/lib/views/NewRequest.svelte
@@ -20,7 +20,7 @@
 		}
 	});
 
-	let requestTypes = ["Congés", "Arrêt maladie", "RTT", "Congés sans solde", "Télétravail", "Exceptionnel", "Accident", "Formation", "Visite"];
+	let requestTypes = ["Congés", "Maladie", "RTT", "Congés sans solde", "Télétravail", "Exceptionnel", "Accident", "Formation", "Visite"];
 	let activeType = "Congés";
 
 	const changeRequestType = (type:string) => {
@@ -40,11 +40,56 @@
 			endMorning = !endMorning;
 		}
 	}
+
+	let myFile = "";
+	let upload: HTMLInputElement;
+
+	function showname(){
+		myFile = upload.files[0].name;
+	}
+
+	let color1 = "#007AFF";
+	let color2 = "#555";
+
+	let frequency : string;
+	let repeat : HTMLElement;
+	let ponctualStart : HTMLElement;
+	let ponctualEnd : HTMLElement;
+
+	let days = [
+		{name: "Lundi",			time: { morning1: false, afternoon1: false}},
+		{name: "Mardi",			time: { morning2: false, afternoon2: false}},
+		{name: "Mercredi",		time: { morning3: false, afternoon3: false}},
+		{name: "Jeudi",			time: { morning4: false, afternoon4: false}},
+		{name: "Vendredi",		time: { morning5: false, afternoon5: false}},
+	];
+
+	const flipColor = (freq : string) => {
+		if(freq != frequency){
+			frequency = freq;
+
+			let tmp = color1;
+			color1 = color2;
+			color2 = tmp;
+			
+			if(frequency == "ponctual"){
+				repeat.style.display = "none";
+				ponctualStart.style.display = "flex";
+				ponctualEnd.style.display = "flex";
+			}
+			else{
+				repeat.style.display = "flex";
+				ponctualStart.style.display = "none";
+				ponctualEnd.style.display = "none";
+			}		
+		}
+	}
+	
 </script>
 
 {#key unique}
 	<Menu active="Demandes"/>
-	<div class=" h-full w-full gap-16" >
+	<div class=" h-full w-full gap-14" >
 		<div class = "flex flex-row">
 			<h1 class = "text-xl mr-5">Créer une demande : </h1>
 
@@ -54,11 +99,62 @@
 				{/each}
 			</select>
 		</div>
-		<div class = "flex flex-row">
+		{#if activeType=="Formation" || activeType=="Visite"}
+			<div class = "flex flex-row">
+				<div class = "w-full">
+					{#if activeType=="Formation"}
+						<label for="nom">Nom de la formation</label>
+					{:else if activeType=="Visite"}
+						<label for="motif">Motif de la visite</label>
+					{/if}	
+					<input type="text" class = "w-80">
+				</div>
+				<div class = "!w-full">
+					{#if activeType=="Formation"}
+						<label for="lieu">Lieu de la formation</label>
+					{:else if activeType=="Visite"}
+						<label for="lieu">Lieu de la visite</label>
+					{/if}
+					<input type="text" class = "w-80">
+				</div>
+			</div>
+		{/if}
+		{#if activeType=="Télétravail"}
+			<div class = "flex flex-row gap-32">
+				<div class = "!w-[30%]">
+					<button id = "ponctuel"
+						class = "!h-[30px] w-[150] !rounded-[25px] !py-0 bg-[{color1}]"
+						on:click={() => flipColor("ponctual")}>ponctuel</button>
+				</div>
+				<div class = "!w-[30%]">
+					<button id = "répétition"
+						class = "!h-[30px] w-[150] !rounded-[25px] !py-0 bg-[{color2}]"
+						on:click={() => flipColor("repetition")}>répétition</button>
+				</div>
+			</div>
+			<div class = "flex-row gap-10 hidden" bind:this={repeat}>
+				{#each days as day}
+					<div class= "!w-[15%] my-4">
+						<label for={day.name}>{day.name}</label>
+						<div class = "flex flex-row !gap-1 my-2">
+							<div>
+								<input type="checkbox" id="matin" bind:checked={day.time[0]}>
+								<label for="matin">matin</label>
+							</div>
+							<div>
+								<input type="checkbox" id="après-midi" bind:checked={day.time[1]}>
+								<label for="après-midi">après-midi</label>
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+		{/if}
+		<div class = "flex flex-row">			
 			<div class = "h-full w-full justify-start">
 				<label for="début">Début</label>
 				<input type="date" id="début" class = "w-52">
-				<div class = "flex flex-row gap-10 my-2">
+				<div class = "flex !flex-row gap-10 my-2" bind:this={ponctualStart}>
 					<span>
 						<input type="checkbox" checked={startMorning} on:change={() => swapCheck("start")}>
 						<label for="matin">matin</label>
@@ -72,7 +168,7 @@
 			<div class = "h-full justify-start">
 				<label for="fin">Fin</label>
 				<input type="date" id="fin" class = "w-52">
-				<div class = "flex flex-row gap-10 my-2">
+				<div class = "flex flex-row gap-10 my-2" bind:this={ponctualEnd}>
 					<span>
 						<input type="checkbox" checked={endMorning} on:change={() => swapCheck("end")}>
 						<label for="matin">matin</label>
@@ -83,20 +179,42 @@
 					</span>
 				</div>
 			</div>
-			<div class = "h-full">
-				<div class = "card h-full !w-3/5">
-					<ul class = "italic font-light">
-					<li>jours acquis : <strong class = "text-[#007AFF]">11</strong></li>
-					<li>jours consommés : <strong class = "text-[#007AFF]">5</strong></li>
-					<li>jours restants : <strong class = "text-[#007AFF]">6</strong></li>
-					<li>jours demandés : <strong class = "text-[#007AFF]">3</strong></li>
-					</ul>
+			{#if activeType=="Congés" || activeType=="RTT" || activeType=="Congés sans solde" || activeType=="Exceptionnel"}
+				<div class = "h-full">
+					<div class = "card h-full !w-3/5">
+						<ul class = "italic font-light">
+						<li>jours acquis : <strong class = "text-[#007AFF]">11</strong></li>
+						<li>jours consommés : <strong class = "text-[#007AFF]">5</strong></li>
+						<li>jours restants : <strong class = "text-[#007AFF]">6</strong></li>
+						<li>jours demandés : <strong class = "text-[#007AFF]">3</strong></li>
+						</ul>
+					</div>
 				</div>
-			</div>
+			{:else if activeType=="Maladie" || activeType=="Accident"}
+				<div class = "h-full">
+					<div class = "h-full !w-[70%] gap-2 border-dotted border-[#a3a3a3] border-[2px]">
+						<header>
+							<h4>Importer un justificatif</h4>
+						</header>
+						<p class = "text-[#a3a3a3] text-[10px]">Fichiers acceptés: PDF, TEXT, DOC , DOCX</p>
+						<input type="file" class = "hidden" accept=".doc,.docx,.pdf" bind:this={upload} on:change={ () => showname() }>
+						<button class="!w-[100px] !h-[30px] bg-[#007AFF] py-0 text-[10px]" on:click={ () => upload.click() }>Parcourir...</button>
+						<p class = "text-[#a3a3a3] text-[10px]">{myFile}</p>
+					</div>
+				</div>
+			{/if}			
 		</div>
-		<div>
-			<label for="commentaire">Commentaire (optionnel)</label>
-			<input type="text" id="commentaire" class = "w-80 h-12">
+		<div class = "flex flex-row">
+			{#if activeType=="Accident"}
+				<div class = "!w-[40%]">
+					<label for="commentaire">Cause de l'accident</label>
+					<input type="text" id="cause" class = "w-80 h-12">
+				</div>
+			{/if}
+			<div class = "!w-[40%]">
+				<label for="commentaire">Commentaire (optionnel)</label>
+				<input type="text" id="commentaire" class = "w-80 h-12">
+			</div>
 		</div>
 		<div class = "flex flex-row gap-28 mt-3">
 			<button class = "bg-[#555]">ANNULER</button>
