6d3e733e5c2acbbb3f96f62e9b1612cb2670979f
Emma Nourry <82103105+emmanry@users.noreply.github.com>
2023-02-16 13:22:49 +0100

    create state tab

--- client/src/lib/shared/utils.ts
+++ client/src/lib/shared/utils.ts
@@ -2,3 +2,16 @@ export function test()
 {
 	console.log('test');
 }
+
+const state = [
+	"Brouillon",
+	"En attente",
+	"En attente de justificatif",
+	"Validé par chef de service",
+	"Réfusé par chef de service",
+	"Validé par RH",
+	"Refusé par RH",
+	"Validé",
+	"Refusé",
+	"Passée"
+]
